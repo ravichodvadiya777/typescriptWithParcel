027d429e54e504d4f37dfd7d1c2d97b0fef73dbb
simpli-hiren-polra <hiren@simpliigence.com>
2024-03-14 17:18:24 +0530

    changes

---
+++ dist/index.377278e2.js
@@ -0,0 +1,17 @@
+alert("bdjshbfjsbdhjhjjbj");
+document.addEventListener("DOMContentLoaded", function() {
+    alert("vdhfvdshf dhvjhd ");
+    const buttonElem = document.getElementById("clickMe");
+    const bodyElem = document.body;
+    if (buttonElem) {
+        let isWhite = false;
+        buttonElem.addEventListener("click", ()=>{
+            console.log("Button clicked");
+            if (isWhite) bodyElem.style.backgroundColor = "";
+            else bodyElem.style.backgroundColor = "#CEDEBD";
+            isWhite = !isWhite;
+        });
+    } else console.error("Button element or body element not found.");
+});
+
+//# sourceMappingURL=index.377278e2.js.map
